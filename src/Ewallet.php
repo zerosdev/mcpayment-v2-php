4918c2eeefff5373e98495c921cc042851cd6daa
ZerosDev <ronywisnuwardana@gmail.com>
2022-10-07 17:23:52 +0700

    add createQR to Ewallet

--- src/Ewallet.php
+++ src/Ewallet.php
@@ -92,4 +92,47 @@ class Ewallet
             'X-Req-Signature' => hash('sha256', $this->client->configs['hash_key'].$payload['external_id'].$payload['order_id'])
         ]);
     }
+
+    public function createQR(array $fields = [])
+    {
+        foreach ($fields as $key => $value) {
+            $this->{$key} = $value;
+        }
+
+        $this->requires([
+            'external_id',
+            'order_id',
+            'currency',
+            'payment_method',
+            'payment_channel',
+            'payment_details',
+            'customer_details',
+            'wallet_details',
+            'callback_url',
+        ]);
+
+        $payload = [
+            'external_id' => $this->external_id,
+            'order_id' => $this->order_id,
+            'currency' => $this->currency,
+            'payment_method' => $this->payment_method,
+            'payment_channel' => $this->payment_channel,
+            'payment_details' => $this->payment_details,
+            'customer_details' => $this->customer_details,
+            'item_details' => $this->item_details,
+            'wallet_details' => $this->wallet_details,
+            'billing_address' => $this->billing_address,
+            'shipping_address' => $this->shipping_address,
+            'additional_data' => $this->additional_data,
+            'callback_url' => $this->callback_url,
+        ];
+
+        $payload = array_filter($payload, function ($p) {
+            return !is_null($p);
+        });
+
+        return $this->client->send('POST', '/ewallet/v2/create-qr', $payload, [
+            'X-Req-Signature' => hash('sha256', $this->client->configs['hash_key'].$payload['external_id'].$payload['order_id'])
+        ]);
+    }
 }
